0dbcfaa961a02e5fd47351761db3aeb7b7ba4887
shashankmishra07 <iamshashankmishra2003@gmail.com>
2024-12-30 23:46:44 +0530

    dfdff

---
+++ bata.js
@@ -0,0 +1,158 @@
+
+let a=false
+function img1(){
+   let img1= document.querySelector('#img1')
+   img1.src="img/8543147_2.jpeg"
+   a=!a
+   if(a){
+    img1.src="img/8543147_2.jpeg"
+   }
+   else{
+    img1.src="img/8543147_1.jpeg"
+   }
+
+
+}
+let b=false
+function img2(){
+    let img2=document.querySelector('#img2')
+    img2.src="img/8396011_2.jpeg"
+    b=!b
+    if(b){
+        img2.src="img/8396011_2.jpeg"
+    }
+    else{
+        img2.src="img/8396011_1.jpeg"
+    }
+}
+
+let c=false
+function img3(){
+    let img3=document.querySelector('#img3')
+    img3.src="img/8597014_2.jpeg"
+    c=!c
+    if(c){
+        img3.src="img/8597014_2.jpeg"
+    }
+    else{
+        img3.src="img/8597014_1.jpeg"
+    }
+}
+
+
+let d=false
+function img4(){
+    let img4=document.querySelector('#img4')
+    img4.src="img/8396212_2.jpeg"
+    d=!d
+    if(d){
+        img4.src="img/8396212_2.jpeg"
+    }
+    else{
+        img4.src="img/8396212_1.jpeg"
+    }
+}
+//parent..................................................
+let e=false
+function img11(){
+    let img11= document.querySelector('#img11')
+    img11.src="img/8516214_2.jpeg"
+    e=!e
+    if(e){
+     img11.src="img/8516214_2.jpeg"
+    }
+    else{
+     img11.src="img/8516214_1.jpeg"
+    }
+ 
+ 
+ }
+
+let f=false
+ function img22(){
+    let img22= document.querySelector('#img22')
+    img22.src=" img/3596240_2.jpeg"
+    f=!f
+    if(f){
+     img22.src=" img/3596240_2.jpeg"
+    }
+    else{
+     img22.src="img/3596240_1.jpeg"
+    }
+ 
+ 
+ }
+
+ let g=false
+ function img33(){
+    let img33= document.querySelector('#img33')
+    img33.src=" img/3595901_2.jpeg"
+    g=!g
+    if(g){
+     img33.src="img/3595901_2.jpeg"
+    }
+    else{
+     img33.src="img/3595901_1.jpeg"
+    }
+ 
+ 
+ }
+
+
+ let h=false
+ function img44(){
+    let img44= document.querySelector('#img44')
+    img44.src=" img/4599913_2.jpeg"
+    h=!h
+    if(h){
+     img44.src="img/4599913_2.jpeg"
+    }
+    else{
+     img44.src="img/4599913_1.jpeg"
+    }
+ 
+ 
+ }
+
+
+ let i=false
+function img55(){
+   let img1= document.querySelector('#img55')
+   img1.src="img/8543147_2.jpeg"
+   i=!i
+   if(i){
+    img1.src="img/8543147_2.jpeg"
+   }
+   else{
+    img1.src="img/8543147_1.jpeg"
+   }
+}
+
+let m=false
+function img66(){
+   let img1= document.querySelector('#img66')
+   img1.src="img/8396011_2.jpeg"
+   m=!m
+   if(m){
+    img1.src="img/8396011_2.jpeg"
+   }
+   else{
+    img1.src="img/8396011_2.jpeg"
+   }
+
+
+}
+
+let w=false
+function demo(){
+    let lastimg=document.querySelector('#lastimg')
+     lastimg.src="img/6514789_2.jpeg"
+    w=!w
+    if(w){
+        lastimg.src="img/8614684_2 (1).jpeg"
+    }
+    else if(w){
+        lastimg.src="img/8332894_1.jpeg"
+    }
+}
+
